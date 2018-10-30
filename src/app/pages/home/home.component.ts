f7443e9763c9025d0e475b8907cb947387739cae
neha <nehasharma.1017@gmail.com>
2018-10-30 09:12:45 +0530

    Initial commit

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
+
+declare var jQuery:any;
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  title: string;
+  desc: string;
+  data: any;
+  titleError:false;
+  descError:false;
+  constructor(private _dataService: DataService) { }
+
+  ngOnInit() {
+    this.getData();
+  }
+  getData() {
+    this._dataService.getData().subscribe(res => {
+      console.log("res-----", res);
+      this.data = res['documents'];
+    }, error => {
+      console.log("Error-----", error);
+    });
+  }
+
+  addItem = function () {
+    let error = false;
+    if(!this.title){
+      this.titleError=true;
+      error =  true
+    }
+    if(!this.desc){
+      this.descError=true;
+      error =  true
+    }
+    if(error){
+        return
+    }
+
+    console.log(this.title + this.desc);
+    let payLoad = {
+      "fields":
+        {
+          "title":
+            {
+              "stringValue": this.title
+            },
+          "description":
+            {
+              "stringValue": this.desc
+            }
+        }
+    }
+    let promiseObj = this._dataService.insertData(payLoad);
+    promiseObj.then((res: any) => {
+      jQuery('#myModal').modal('hide');
+      this.getData();
+      console.log("inserted-----", res);
+    });
+  }
+
+
+  updateState(event){
+    this[event.target.name+'Error'] = false;
+  }
+
+  deleteData = function (name) {
+    let id = name.split('/');
+    id = id[id.length - 1]
+    let promiseObj = this._dataService.deleteData(id);
+    promiseObj.then((res: any) => {
+      this.getData();
+      console.log("deleted-----", res);
+    });
+  }
+
+}
